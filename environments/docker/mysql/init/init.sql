e82bd4821f91e6902ceaf4bc7e93af6b1b020d29
yy_yank <yy.yank.me@gmail.com>
2021-10-19 13:21:50 +0900

    setup

---
+++ environments/docker/mysql/init/init.sql
@@ -0,0 +1,11 @@
+# In production you would almost certainly limit the replication user must be on the follower (slave) machine,
+# to prevent other clients accessing the log from other machines. For example, 'replicator'@'follower.acme.com'.
+#
+# However, this grant is equivalent to specifying *any* hosts, which makes this easier since the docker host
+# is not easily known to the Docker container. But don't do this in production.
+#
+GRANT REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replicator' IDENTIFIED BY 'replpass';
+GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT  ON *.* TO 'debezium' IDENTIFIED BY 'dbz';
+
+# Create the database that we'll use to populate data and watch the effect in the binlog
+GRANT ALL PRIVILEGES ON `debezium-sample.*` TO 'mysqluser'@'%';
