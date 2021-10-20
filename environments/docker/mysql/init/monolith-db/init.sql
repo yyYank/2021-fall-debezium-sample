0e872efd736e7f375bf9c603a88f9f7c5e9fcefe
yy_yank <yy.yank.me@gmail.com>
2021-10-20 11:50:55 +0900

    monolithとalbum api用のDBを立てた

--- environments/docker/mysql/init/monolith-db/init.sql
+++ environments/docker/mysql/init/monolith-db/init.sql
@@ -9,3 +9,20 @@ GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT  ON 
 
 # Create the database that we'll use to populate data and watch the effect in the binlog
 GRANT ALL PRIVILEGES ON `debezium-sample.*` TO 'mysqluser'@'%';
+
+use debezium-sample
+create table albums
+(
+    id         bigint auto_increment primary key,
+    name       varchar(255) not null,
+    created_at timestamp    not null default current_timestamp
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+create table tracks
+(
+    id         bigint auto_increment primary key,
+    no         int,
+    name       varchar(255) not null,
+    review     int,
+    created_at timestamp    not null default current_timestamp
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
