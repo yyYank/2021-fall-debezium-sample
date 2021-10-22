5afeb462c06a1a8ffdd55cb4e72db98ff8eeec65
yy_yank <yy.yank.me@gmail.com>
2021-10-22 16:27:17 +0900

    import修正

--- album-api/src/main/kotlin/com/github/yyyank/album/service/TracksService.kt
+++ album-api/src/main/kotlin/com/github/yyyank/album/service/TracksService.kt
@@ -1,9 +1,7 @@
 package com.github.yyyank.album.service
 
-import CreateTracksRequest
-import UpdateTracksRequest
-import com.github.yyyank.album.repository.Album
-import com.github.yyyank.album.repository.AlbumsRepository
+import com.github.yyyank.album.controller.CreateTracksRequest
+import com.github.yyyank.album.controller.UpdateTracksRequest
 import com.github.yyyank.album.repository.Track
 import com.github.yyyank.album.repository.TracksRepository
 import org.springframework.stereotype.Service
